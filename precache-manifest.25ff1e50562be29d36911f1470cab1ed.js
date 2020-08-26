6f5eaa9521f964bda8b3e8e85618a8fd5e10a2e7
jvnaveenbabu <naveenbabulucky264@gmail.com>
2020-08-27 01:42:32 +0530

    Updates

---
+++ precache-manifest.25ff1e50562be29d36911f1470cab1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309a9ae52ae7793d4b17b4acede235b5",
+    "url": "/Color-Fetcher/index.html"
+  },
+  {
+    "revision": "50abf9e500c80f77afdc",
+    "url": "/Color-Fetcher/static/css/main.38332eff.chunk.css"
+  },
+  {
+    "revision": "a54a0131516b19a6be11",
+    "url": "/Color-Fetcher/static/js/2.1bc976a1.chunk.js"
+  },
+  {
+    "revision": "62afbf7b6a84202ba7762582324f8a1a",
+    "url": "/Color-Fetcher/static/js/2.1bc976a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50abf9e500c80f77afdc",
+    "url": "/Color-Fetcher/static/js/main.dadcf328.chunk.js"
+  },
+  {
+    "revision": "e3ac9f892b6c2a232032",
+    "url": "/Color-Fetcher/static/js/runtime-main.a129cb5c.js"
+  },
+  {
+    "revision": "d392c03aafbd1b767e473452fe070aea",
+    "url": "/Color-Fetcher/static/media/click.d392c03a.mp3"
+  }
+]);
